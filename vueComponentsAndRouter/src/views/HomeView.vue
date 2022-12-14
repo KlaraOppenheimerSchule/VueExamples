d7ea11edf3319eeae6b16dd9a85a28b29136c4c3
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2022-12-14 09:52:08 +0100

    name change

---
+++ vueComponentsAndRouter/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <h1> Home View</h1>
+  <p> Central information home view </p>
+  <Footer />
+</template>
+
